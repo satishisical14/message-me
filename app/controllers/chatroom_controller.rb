0c2edf7c2fea67d802a35ec4114a32c96f7308b2
satishisical <satish.isical14@gmail.com>
2019-04-19 20:58:32 +0530

    initial setup

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,5 @@
+class ChatroomController < ApplicationController
+	def index
+		render 'index'
+	end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ApplicationController
+	def index
+		render :index
+	end
+end
